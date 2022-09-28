904918999859adb5354fb4d8de642449a1b47e06
Your Name <you@example.com>
2022-09-27 22:01:40 -0600

    completely setup

---
+++ pages/components/home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./home.module.css"
+export default function Home() {
+    return(<>
+        <body className={styles.background}>
+            <h1>Raiders of the Lost Parts</h1>
+            <h3>test</h3>
+        </body>
+    </>)
+    
+}
